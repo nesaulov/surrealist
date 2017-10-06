9f311a380ea5b954de9a62b73da5eab48ecdf38d
Nikita Esaulov <billikota@gmail.com>
2017-10-06 19:49:41 +0300

    Filter out specs from coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require 'coveralls'
+
+SimpleCov.start do
+  filters.clear
+
+  add_filter do |src|
+    src.filename =~ /^#{SimpleCov.root}\/lib/
+  end
+end
+
 Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
