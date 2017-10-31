7f1f17cb4f53e84533a5a3d533ff2e3d6e109740
AlessandroMinali <alessandro.minali@gmail.com>
2017-10-31 15:04:46 -0400

    freeze strings

--- lib/surrealist/helper.rb
+++ lib/surrealist/helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Surrealist
   # A generic helper.
   class Helper
--- spec/nested_record_spec.rb
+++ spec/nested_record_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../lib/surrealist'
 require_relative './orms/ar'
 require_relative './carriers/params'
