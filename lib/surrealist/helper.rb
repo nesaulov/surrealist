bc5208df4ff26353aeb340138f580a5fd319a63f
Nikita Esaulov <billikota@gmail.com>
2017-11-25 00:27:48 +0300

    `class` -> `module` where no instances are created

--- lib/surrealist/helper.rb
+++ lib/surrealist/helper.rb
@@ -2,16 +2,14 @@
 
 module Surrealist
   # A generic helper.
-  class Helper
-    class << self
-      # Determines if the class uses the Surrealist mixin.
-      #
-      # @param [Class] klass a class to be checked.
-      #
-      # @return [Boolean] if Surrealist is included in class.
-      def surrealist?(klass)
-        klass < Surrealist
-      end
+  module Helper
+    # Determines if the class uses the Surrealist mixin.
+    #
+    # @param [Class] klass a class to be checked.
+    #
+    # @return [Boolean] if Surrealist is included in class.
+    def self.surrealist?(klass)
+      klass < Surrealist
     end
   end
 end
