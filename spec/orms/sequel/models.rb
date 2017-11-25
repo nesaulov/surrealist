424bba90446bc952ede8dec9245384a998c864d0
Nikita Esaulov <billikota@gmail.com>
2017-11-25 19:08:55 +0300

    Add specs for one-to-many and many-to-one associations

--- spec/orms/sequel/models.rb
+++ spec/orms/sequel/models.rb
@@ -2,16 +2,37 @@ require 'sequel'
 
 DB = Sequel.sqlite
 
-DB.create_table :sequel_items do
+DB.create_table :artists do
   primary_key :id
   String :name
-  Float :price
+  Float :age
 end
 
-class SequelItem < Sequel::Model
+DB.create_table :albums do
+  primary_key :id
+  Integer :artist_id
+  String :title
+  Integer :year
+end
+
+class Artist < Sequel::Model
   include Surrealist
+
+  one_to_many :albums
 
   json_schema { { name: String } }
 end
 
-7.times { |i| SequelItem.insert(name: "SequelItem #{i}", price: i * 4) }
+class Album < Sequel::Model
+  include Surrealist
+
+  many_to_one :artist
+
+  json_schema { { title: String, year: Integer } }
+end
+
+7.times { |i| Artist.insert(name: "Artist #{i}", age: i * 4) }
+
+Artist.each_with_index do |artist, i|
+  artist.add_album(title: "Album #{i}", year: (1950 + i * 5))
+end
--- spec/orms/sequel/sequel_spec.rb
+++ spec/orms/sequel/sequel_spec.rb
@@ -3,60 +3,63 @@
 require_relative 'models'
 
 RSpec.describe 'Sequel integration' do
+  # Basically, Sequel returns instance only on #first, #last and #[].
+  # And instances are returned for all other methods.
   describe 'instances' do
     let(:subject) { instance.surrealize }
 
     describe '#first' do
-      let(:instance) { SequelItem.first }
+      let(:instance) { Artist.first }
 
-      it { is_expected.to eq({ name: 'SequelItem 0' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 0' }.to_json) }
       it_behaves_like 'error is not raised for valid params: instance'
       it_behaves_like 'error is raised for invalid params: instance'
     end
 
     describe '#first({ condition })' do
-      let(:instance) { SequelItem.first(price: 4) }
+      let(:instance) { Artist.first(age: 4) }
 
-      it { is_expected.to eq({ name: 'SequelItem 1' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 1' }.to_json) }
     end
 
     describe '#[]' do
-      let(:instance) { SequelItem[3] }
+      let(:instance) { Artist[3] }
 
-      it { is_expected.to eq({ name: 'SequelItem 2' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 2' }.to_json) }
     end
 
     describe '#[{ condition }]' do
-      let(:instance) { SequelItem[{ price: 12 }] }
+      let(:instance) { Artist[{ age: 12 }] }
 
-      it { is_expected.to eq({ name: 'SequelItem 3' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 3' }.to_json) }
     end
 
     describe '#with_pk!' do
-      let(:instance) { SequelItem.with_pk!(5) }
+      let(:instance) { Artist.with_pk!(5) }
 
-      it { is_expected.to eq({ name: 'SequelItem 4' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 4' }.to_json) }
     end
 
     describe '#last({ condition })' do
-      let(:instance) { SequelItem.last(price: 20) }
+      let(:instance) { Artist.last(age: 20) }
 
-      it { is_expected.to eq({ name: 'SequelItem 5' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 5' }.to_json) }
     end
 
     describe '#last' do
-      let(:instance) { SequelItem.last }
+      let(:instance) { Artist.last }
 
-      it { is_expected.to eq({ name: 'SequelItem 6' }.to_json) }
+      it { is_expected.to eq({ name: 'Artist 6' }.to_json) }
     end
   end
 
   describe 'collections' do
     let(:subject) { Surrealist.surrealize_collection(collection) }
+    let(:all) { Array.new(7) { |i| { name: "Artist #{i}" } } }
+    let(:serialized_collection) { all.to_json }
 
     describe '#all' do
-      let(:collection) { SequelItem.all }
-      let(:serialized_collection) { (Array.new(7) { |i| { name: "SequelItem #{i}" } }).to_json }
+      let(:collection) { Artist.all }
 
       it { is_expected.to eq(serialized_collection) }
       it_behaves_like 'error is not raised for valid params: collection'
@@ -65,52 +68,102 @@ RSpec.describe 'Sequel integration' do
 
     # In Sequel #where always returns an array
     describe '#where()' do
-      let(:collection) { SequelItem.where(id: 2) }
-      let(:serialized_collection) { [name: 'SequelItem 1'].to_json }
+      let(:collection) { Artist.where(id: 2) }
+      let(:serialized_collection) { [name: 'Artist 1'].to_json }
 
       it { is_expected.to eq(serialized_collection) }
       it_behaves_like 'error is not raised for valid params: collection'
       it_behaves_like 'error is raised for invalid params: collection'
 
       context 'with #select' do
-        let(:collection) { SequelItem.select(:id, :name).order(:name).where(id: 2) }
+        let(:collection) { Artist.select(:id, :name).order(:name).where(id: 2) }
 
         it { is_expected.to eq(serialized_collection) }
       end
 
       context 'with #all' do
-        let(:collection) { SequelItem.where(id: 2).all }
+        let(:collection) { Artist.where(id: 2).all }
 
         it { is_expected.to eq(serialized_collection) }
+      end
+
+      context 'with #select and necessary field not selected' do
+        let(:collection) { Artist.select(:id, :age).where(id: 2) }
+        let(:serialized_collection) { [name: nil].to_json }
+
+        it('substitutes `null` as value') { is_expected.to eq(serialized_collection) }
       end
     end
 
     describe '#where{}' do
-      let(:collection) { SequelItem.where { price < 3 } }
-      let(:serialized_collection) { [name: 'SequelItem 0'].to_json }
+      let(:collection) { Artist.where { age < 3 } }
+      let(:serialized_collection) { [name: 'Artist 0'].to_json }
 
       it { is_expected.to eq(serialized_collection) }
       it_behaves_like 'error is not raised for valid params: collection'
       it_behaves_like 'error is raised for invalid params: collection'
 
       context 'with #select' do
-        let(:collection) { SequelItem.select(:id, :name).where { price < 3 } }
+        let(:collection) { Artist.select(:id, :name).where { age < 3 } }
 
         it { is_expected.to eq(serialized_collection) }
       end
 
       context 'with #all' do
-        let(:collection) { SequelItem.where { price < 3 }.all }
+        let(:collection) { Artist.where { age < 3 }.all }
+
+        it { is_expected.to eq(serialized_collection) }
+      end
+
+      context 'with #select and necessary field not selected' do
+        let(:collection) { Artist.select(:id, :age).where { age < 3 } }
+        let(:serialized_collection) { [name: nil].to_json }
+
+        it('substitutes `null` as value') { is_expected.to eq(serialized_collection) }
+      end
+    end
+
+    describe 'ordering methods' do
+      context '#order' do
+        let(:collection) { Artist.order(:age) }
+
+        it { is_expected.to eq(serialized_collection) }
+      end
+
+      context '#reverse' do
+        let(:collection) { Artist.reverse(:age) }
+        let(:serialized_collection) { all.reverse.to_json }
 
         it { is_expected.to eq(serialized_collection) }
       end
     end
+  end
 
-    describe 'with #select and necessary field not selected' do
-      let(:collection) { SequelItem.select(:id, :price).where(id: 2) }
-      let(:serialized_collection) { [name: nil].to_json }
+  describe 'associations' do
+    describe 'many to one' do
+      let(:serialized_instance) { { name: 'Artist 0' }.to_json }
 
-      it('substitutes `null` as value') { is_expected.to eq(serialized_collection) }
+      context '#first' do
+        let(:instance) { Album.first.artist }
+
+        it { expect(instance.surrealize).to eq(serialized_instance) }
+        it_behaves_like 'error is not raised for valid params: instance'
+        it_behaves_like 'error is raised for invalid params: instance'
+      end
+
+      context 'where()' do
+        let(:instance) { Album.where(id: 1).first.artist }
+
+        it { expect(instance.surrealize).to eq(serialized_instance) }
+      end
+    end
+
+    describe 'one to many' do
+      let(:subject) { Surrealist.surrealize_collection(collection) }
+      let(:collection) { Artist.first.albums }
+      let(:serialized_collection) { [title: 'Album 0', year: 1950].to_json }
+
+      it { is_expected.to eq(serialized_collection) }
     end
   end
 end
