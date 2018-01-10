bd34f2415655965e71feca57a42f2a07832589c3
Nikita Esaulov <billikota@gmail.com>
2018-01-10 23:02:04 +0300

    Add more specs

--- spec/serializer_spec.rb
+++ spec/serializer_spec.rb
@@ -7,7 +7,7 @@ class DogeSerializer < Surrealist::Serializer
     { name: String, name_length: Integer }
   end
 
-  def name_length
+  private def name_length
     name.length
   end
 end
@@ -27,27 +27,42 @@ RSpec.describe Surrealist::Serializer do
   describe 'Explicit surrealization through `Serializer.new`' do
     describe 'instance' do
       let(:instance) { Doge.new('John') }
+      let(:expectation) { { name: 'John', name_length: 4 }.to_json }
       subject(:json) { DogeSerializer.new(instance).surrealize }
 
-      it { is_expected.to eq({ name: 'John', name_length: 4 }.to_json) }
-      # it_behaves_like 'error is raised for invalid params: instance'
-      # it_behaves_like 'error is not raised for valid params: instance'
+      it { is_expected.to eq(expectation) }
+      it_behaves_like 'error is raised for invalid params: instance'
+      it_behaves_like 'error is not raised for valid params: instance'
     end
 
     describe 'collection' do
       let(:collection) { [Doge.new('John'), Doge.new('Josh')] }
+      let(:expectation) { [{ name: 'John', name_length: 4 }, { name: 'Josh', name_length: 4 }].to_json }
       subject(:json) { DogeSerializer.new(collection).surrealize }
 
-      it 'works' do
-        expect(json)
-          .to eq([{ name: 'John', name_lengh: 4 }, { name: 'Josh', name_lenght: 4 }].to_json)
-      end
+      it { is_expected.to eq(expectation) }
+      it_behaves_like 'error is raised for invalid params: collection'
+      it_behaves_like 'error is not raised for valid params: collection'
     end
   end
 
   describe 'Implicit surrealization using .surrealize_with' do
-    let(:cat) { Doge.new('George').surrealize }
+    describe 'instance' do
+      let(:instance) { Doge.new('George') }
+      let(:expectation) { { name: 'George', name_length: 6 }.to_json }
+      subject(:json) { instance.surrealize }
 
-    it { expect(cat).to eq({ name: 'George', name_length: 6 }.to_json) }
+      it { is_expected.to eq(expectation) }
+      it_behaves_like 'error is raised for invalid params: instance'
+      it_behaves_like 'error is not raised for valid params: instance'
+    end
+
+    describe 'collection' do
+      let(:collection) { [Doge.new('Wow'), Doge.new('Doge')] }
+      let(:expectation) { [{ name: 'Wow', name_length: 3 }, { name: 'Doge', name_length: 4 }].to_json }
+      subject(:json) { Surrealist.surrealize_collection(collection) }
+
+      it { is_expected.to eq(expectation) }
+    end
   end
 end
