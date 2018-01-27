575d8d55561e5b66b4aafe9df72dae736f33bfde
Nikita Esaulov <billikota@gmail.com>
2018-01-27 17:13:10 +0300

    Add benchmarks for AMS logger turned on/off

--- benchmarks/surrealist_vs_ams.rb
+++ benchmarks/surrealist_vs_ams.rb
@@ -10,8 +10,6 @@ ActiveRecord::Base.establish_connection(
 )
 
 ActiveRecord::Migration.verbose = false
-ActiveModelSerializers.logger.level = Logger::Severity::UNKNOWN
-
 ActiveRecord::Schema.define do
   create_table :users do |table|
     table.column :name, :string
@@ -41,16 +39,14 @@ N = 3000
 
 N.times { User.create!(name: random_name, email: "#{random_name}@test.com") }
 
-Benchmark.ips do |x|
-  # Instance serialization
-  user = User.find(rand(1..N))
-  instance_ams_serializer = ActiveModelSerializers::SerializableResource.new(user)
-  instance_surrealist_serializer = UserSurrealistSerializer.new(user)
+user = User.find(rand(1..N))
+users = User.all
 
+Benchmark.ips do |x|
   x.config(time: 5, warmup: 2)
 
   x.report('AMS: instance') do
-    instance_ams_serializer.to_json
+    ActiveModelSerializers::SerializableResource.new(user).to_json
   end
 
   x.report('Surrealist: instance through .surrealize') do
@@ -58,22 +54,17 @@ Benchmark.ips do |x|
   end
 
   x.report('Surrealist: instance through Surrealist::Serializer') do
-    instance_surrealist_serializer.surrealize
+    UserSurrealistSerializer.new(user).surrealize
   end
 
   x.compare!
 end
 
 Benchmark.ips do |x|
-  # Collection serialization
-  users = User.all
-  coll_ams_serializer = ActiveModelSerializers::SerializableResource.new(users)
-  coll_surrealist_serializer = UserSurrealistSerializer.new(users)
-
   x.config(time: 5, warmup: 2)
 
   x.report('AMS: collection') do
-    coll_ams_serializer.to_json
+    ActiveModelSerializers::SerializableResource.new(users).to_json
   end
 
   x.report('Surrealist: collection through Surrealist.surrealize_collection()') do
@@ -81,8 +72,77 @@ Benchmark.ips do |x|
   end
 
   x.report('Surrealist: collection through Surrealist::Serializer') do
-    coll_surrealist_serializer.surrealize
+    UserSurrealistSerializer.new(users).surrealize
   end
 
   x.compare!
 end
+
+
+# With AMS logger turned off
+#
+puts "\n------- Turning off AMS logger -------\n"
+ActiveModelSerializers.logger.level = Logger::Severity::UNKNOWN
+
+Benchmark.ips do |x|
+  x.config(time: 5, warmup: 2)
+
+  x.report('AMS (without logging): instance') do
+    ActiveModelSerializers::SerializableResource.new(user).to_json
+  end
+
+  x.report('Surrealist: instance through .surrealize') do
+    user.surrealize
+  end
+
+  x.report('Surrealist: instance through Surrealist::Serializer') do
+    UserSurrealistSerializer.new(user).surrealize
+  end
+
+  x.compare!
+end
+
+Benchmark.ips do |x|
+  x.config(time: 5, warmup: 2)
+
+  x.report('AMS (without logging): collection') do
+    ActiveModelSerializers::SerializableResource.new(users).to_json
+  end
+
+  x.report('Surrealist: collection through Surrealist.surrealize_collection()') do
+    Surrealist.surrealize_collection(users)
+  end
+
+  x.report('Surrealist: collection through Surrealist::Serializer') do
+    UserSurrealistSerializer.new(users).surrealize
+  end
+
+  x.compare!
+end
+
+
+# -- Instance --
+# Comparison:
+# Surrealist: instance through .surrealize:    18486.8 i/s
+# Surrealist: instance through Surrealist::Serializer:    15289.3 i/s - same-ish: difference falls within error
+# AMS: instance:     2402.3 i/s - 7.70x  slower
+
+# -- Collection --
+# Comparison:
+# Surrealist: collection through Surrealist.surrealize_collection():        7.2 i/s
+# Surrealist: collection through Surrealist::Serializer:        6.7 i/s - same-ish: difference falls within error
+# AMS: collection:        5.9 i/s - same-ish: difference falls within error
+
+# --- Without AMS logging ---
+#
+# -- Instance --
+# Comparison:
+# Surrealist: instance through .surrealize:    19634.9 i/s
+# Surrealist: instance through Surrealist::Serializer:    18273.5 i/s - same-ish: difference falls within error
+# AMS (without logging): instance:     6570.4 i/s - 2.99x  slower
+
+# -- Collection --
+# Comparison:
+# Surrealist: collection through Surrealist.surrealize_collection():        7.2 i/s
+# Surrealist: collection through Surrealist::Serializer:        7.0 i/s - same-ish: difference falls within error
+# AMS (without logging): collection:        5.6 i/s - same-ish: difference falls within error
