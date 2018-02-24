c8d1e6ad8fa17e6da254afc2b808f30d331066bd
Nikita Esaulov <billikota@gmail.com>
2018-02-24 23:15:39 +0300

    Add specs for alias & invalid arguments

--- spec/serializer_context_spec.rb
+++ spec/serializer_context_spec.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../lib/surrealist'
-
-TestSerializerClass = Class.new(Surrealist::Serializer) do
-  serializer_context :user
-  json_schema { { id_num: Integer, user_name: String } }
-
-  def id_num
-    2
-  end
-
-  def user_name
-    user.name
-  end
-end
-
-Multiple = Class.new(Surrealist::Serializer) do
-  serializer_context :user, :mouse, nil, :cow
-  json_schema { { user_name: String, mouse_name: String, cow_name: String } }
-
+Names = Module.new do
   def user_name
     user.name
   end
@@ -32,6 +14,23 @@ Multiple = Class.new(Surrealist::Serializer) do
   end
 end
 
+TestSerializerClass = Class.new(Surrealist::Serializer) do
+  prepend Names
+
+  serializer_context :user
+  json_schema { { id_num: Integer, user_name: String } }
+
+  def id_num
+    2
+  end
+end
+
+Multiple = Class.new(Surrealist::Serializer) do
+  prepend Names
+  serializer_context :user, :mouse, :cow
+  json_schema { { user_name: String, mouse_name: String, cow_name: String } }
+end
+
 Cow   = Struct.new(:name)
 Mouse = Struct.new(:name)
 
@@ -43,10 +42,17 @@ User = Class.new do
   end
 end
 
-RSpec.describe Surrealist::Serializer do
-  describe '.serializer_context' do
-    let(:user) { User.new('Dave') }
+Plural = Class.new(Surrealist::Serializer) do
+  prepend Names
+  serializer_contexts :user, :mouse, :cow
+  json_schema { { user_name: String, mouse_name: String, cow_name: String } }
+end
 
+RSpec.describe Surrealist::Serializer do
+  let(:user) { User.new('Dave') }
+  let(:args) { { mouse: Mouse.new('Pes'), cow: Cow.new('Cat'), user: user } }
+
+  describe '.serializer_context' do
     context 'one method' do
       let(:instance) { TestSerializerClass.new(nil, **args) }
       let(:args) { { user: user } }
@@ -56,7 +62,32 @@ RSpec.describe Surrealist::Serializer do
 
     context 'many methods' do
       let(:instance) { Multiple.new(nil, **args) }
-      let(:args) { { mouse: Mouse.new('Pes'), cow: Cow.new('Cat'), user: user } }
+
+      it { expect(instance.build_schema).to eq(user_name: 'Dave', mouse_name: 'Pes', cow_name: 'Cat') }
+    end
+
+    context 'with .serializer_context defined, but context not passed' do
+      let(:instance) { Multiple.new(nil) }
+
+      it { expect { instance.build_schema }.to raise_error(Surrealist::UndefinedMethodError) }
+    end
+
+    describe 'invalid arguments' do
+      ['string', nil, 2, Class, {}, []].each do |argument|
+        it "raises ArgumentError for #{argument}" do
+          expect do
+            Class.new(Surrealist::Serializer) do
+              serializer_context argument
+            end
+          end.to raise_error(ArgumentError, 'Please provide an array of symbols to `.serializer_context`')
+        end
+      end
+    end
+  end
+
+  describe '.serializer_contexts' do
+    context 'many methods' do
+      let(:instance) { Plural.new(nil, **args) }
 
       it { expect(instance.build_schema).to eq(user_name: 'Dave', mouse_name: 'Pes', cow_name: 'Cat') }
     end
