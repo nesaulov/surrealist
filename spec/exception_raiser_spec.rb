11e8265d55b78c3018a3967d38561f4dc54ee601
Alexander Komarov <ak@akxcv.com>
2018-10-12 17:33:56 +0300

    Preserve exception backtrace in case of Surrealist::UndefinedMethodError (#122)
    * Preserve backtrace in Surrealist::UndefinedMethodError
    
    * add spec

---
+++ spec/exception_raiser_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe Surrealist::ExceptionRaiser do
+  describe '.raise_invalid_key!' do
+    let(:backtrace) { %w[a b c] }
+
+    it 'preserves exception backtrace and displays correct message' do
+      begin
+        raise NoMethodError, 'my error message', backtrace
+      rescue NoMethodError => e
+        begin
+          described_class.raise_invalid_key!(e)
+        rescue Surrealist::UndefinedMethodError => e
+          expect(e.message).to eq(
+            "my error message. " \
+            "You have probably defined a key " \
+            "in the schema that doesn't have a corresponding method.",
+          )
+          expect(e.backtrace).to eq(backtrace)
+        end
+      end
+    end
+  end
+end
